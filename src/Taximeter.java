23e3aecbe53d2e9435a965ee88c8da01b820fc10
Burcin Erden <burcnerden@gmail.com>
2022-01-10 17:09:54 +0300

    Taximeter projesi oluşturuldu

---
+++ src/Taximeter.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Taximeter {
+    public static void main(String[] args) {
+        double km,perKm=2.20,startPrice=10.0,total;
+        Scanner distance=new Scanner(System.in);
+        System.out.println("Taximeter starting price is:"+startPrice);
+        System.out.println("Enter the distanance in km:");
+        km= distance.nextDouble();
+        total=startPrice+(km*2.20);
+        total=(total<20)? 20: total;
+        System.out.print("The amount you have to pay:"+total);
+
+
+    }
+
+}
