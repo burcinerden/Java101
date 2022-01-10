f154d58005d386a41652b8450233a2ea639d06ce
Burcin Erden <burcnerden@gmail.com>
2022-01-10 22:00:14 +0300

    BodyIndex projesi oluşturuldu

---
+++ src/BodyIndex.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class BodyIndex {
+    public static void main(String[]args){
+
+        double index,height,weight;
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Enter the your weight(kg):");
+        weight=inp.nextDouble();
+        System.out.print("Enter the your height(m):");
+        height=inp.nextDouble();
+        index=weight/(height*height);
+        System.out.println("Your body index is:"+index);
+
+    }
+
+}
