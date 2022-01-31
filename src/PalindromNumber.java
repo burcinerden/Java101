09adb8874edb379f3e0b98cbc301879927aa7f84
Burcin Erden <burcnerden@gmail.com>
2022-01-31 17:06:56 +0300

    PalindromNumber projesi oluşturldu

---
+++ src/PalindromNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class PalindromNumber {
+
+    static int isPalindrom(int number) {
+        int temp = number, reverseNumber = 0, lastNumber;
+        while ((temp != 0)) {
+            //System.out.println("Sayı=>" + temp);
+            lastNumber = temp % 10;
+           // System.out.println("Son basamak=>" + lastNumber);
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+           // System.out.println("Yeni sayı=>" + reverseNumber);
+            temp /= 10;
+            //System.out.println("----------------------------");
+        }
+        if(number==reverseNumber){
+            System.out.println("Palindrom");
+        }else{
+            System.out.println("Not palindrom");
+        }
+        return 1;
+    }
+    public static void main(String[]args){
+        int n;
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Enter a number:");
+        n= scan.nextInt();
+        isPalindrom(n);
+
+    }
+}
